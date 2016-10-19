ba1ff738d3c3c54aaadb2fe9034823dca947e166
mitty.q <mitty.q@outlook.com>
2016-10-19 09:08:59 +0800

    ms

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var uglifycss = require('gulp-uglifycss');
+
+gulp.task('css', function () {
+    gulp.src('./source/**/*.css')
+            .pipe(uglifycss({
+                "maxLineLen": 80,
+                "uglyComments": true
+            }))
+            .pipe(gulp.dest('./dist/'));
+});
